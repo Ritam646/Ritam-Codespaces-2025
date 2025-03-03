47357161ff9a18ceae5d7b7ea51fa535ec73ac28
Ritam646 <146725244+Ritam646@users.noreply.github.com>
2025-03-03 19:22:35 +0000

    basics on js

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 14453
+let accountEmail = "ritam@gmail.com"
+var accountPassword = "12345"
+accountCity = "Kolkata"
+let accountState;
+
+// accountId = 2 // not allowed in javascfriipt as it is a constant 
+
+accountEmail = "rp@rp.com"
+accountPassword = "21231"
+accountCity = "Bengaluru"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+//alert(3+3) // we are using node js not js so it makes a error
+
+console.log(3+3)
+
+let name = "ritam";
+let age = 21;
+let islogged = false;
+let state;
+
+console.log(typeof(age));
+console.log(typeof(name));
+console.log(typeof(state));
+console.log(typeof null);
