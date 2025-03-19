7ab34bf99a62c6303492a37c0f828484eaee6c18
Ritam646 <146725244+Ritam646@users.noreply.github.com>
2025-03-19 18:16:31 +0000

    add new

---
+++ Coding-Language-Tutorial /C Tutorial/hello.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("hello world!");
+    printf("/n");
+    printf("%d",a);
+    a = 5;
+    b = 6;
+    printf("%d",a);
+    printf("/n");
+    printf("%d",a+b);
+    return 0;
+}
