c184f265e6d0fec18fb2265e435618d789848065
Ritam646 <146725244+Ritam646@users.noreply.github.com>
2025-04-18 10:43:31 +0000

    add new typescript

---
+++ Coding-Language-Tutorial /Typescript Tutorial/app.js
@@ -0,0 +1,11 @@
+var a = 6;
+console.log(a);
+function abcd(val) {
+    console.log("hello world");
+    var sum = 0;
+    for (var i = 0; i < 6; i++) {
+        sum = val + i;
+    }
+    console.log(sum);
+}
+abcd(12);
---
+++ Coding-Language-Tutorial /Typescript Tutorial/app.ts
@@ -0,0 +1,14 @@
+var a = 6;
+console.log(a);
+
+function abcd(val: number){
+    console.log("hello world");
+    let sum = 0
+    for (let i = 0;i < 6;i++) {
+        sum = val + i
+        
+    }
+    console.log(sum)
+}
+
+abcd(12)
